0fc41a7c7a9f191583918659251c540331dd8143
mariyatarasenko88 <tmv1231@yandex.ru>
2023-05-11 22:00:43 +0300

    Initial commit

---
+++ src/main/java/pro/sky/webcalculator2/WebCalculator2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.webcalculator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculator2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCalculator2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/webcalculator2/WebCalculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.webcalculator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculator2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
